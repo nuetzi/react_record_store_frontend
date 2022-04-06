3f508cd8208f2416e454c3ef549bf8b436cc412c
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-04-06 00:08:44 -0400

    Index page coded (I think?)

---
+++ src/pages/Index.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { getItem } from "../services/APIFunctions";
+
+export default function Index() {
+    const [album, setAlbum] = useState([]);
+
+    useEffect(() => {
+        getItem()
+        .then(res => setAlbum(res.data))
+    }, []);
+
+    return(
+        <div>
+            <h2>All Albums</h2>
+            <div className="albumContainer">
+                {album.map((each, i) => {
+                    return(
+                        <div className="albumsDisplay" key={i}>
+                            <a href={`/${each._id}`}><h3>each.title</h3></a>
+                            <h4>Artist: {each.artist} <br/>
+                            Year: {each.year} <br/>
+                            Genre: {each.genre}</h4>
+                            <img src={each.image} />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
